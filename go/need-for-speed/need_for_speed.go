dd52322bbb92e3b5cad831865019e7974ab48a36
Christopher Chalcraft <chris.chalcraft@gmail.com>
2022-10-07 00:08:02 -0700

    first kick

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,42 @@
+package speed
+
+// speed and battery drain
+// initial battery = 100%
+// threshold % no drive
+// TODO: define the 'Car' type struct
+type Car struct {
+	battery, batteryDrain, speed, distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		batttery:     100,
+		batteryDrain: batterDrain,
+		speed:        speed,
+		distance:     0,
+	}
+}
+
+// TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	return Car{}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	panic("Please implement the CanFinish function")
+}
